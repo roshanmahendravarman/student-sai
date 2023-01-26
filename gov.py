0dbac4fe3b816627a85aa88c830af7280025106b
roshanmahendravarman <123367901+roshanmahendravarman@users.noreply.github.com>
2023-01-26 15:31:47 +0530

    Update gov.py

--- gov.py
+++ gov.py
@@ -5,34 +5,17 @@ import warnings
 from pandas import read_csv
 from pandas import datetime
     
-st.title('**HEART FAILURE PREDICTION**')
-st.markdown("**MANISHA WELCOME YOU TO THIS WEB APP**") 
+st.title('**BUS ANALYSIS**')
+st.markdown("**welcome**") 
 st.write("""
-         **The health care industries collect huge amounts of data that contain some hidden information, which
-         is useful for making effective decisions. For providing appropriate results and making effective decisions
-         on data, some advanced data mining techniques are used. In this study, an effective heart disease prediction system 
-         (EHDPS) is developed using neural network for predicting the risk level of heart disease. The system uses 15 medical
-         parameters such as age, sex, blood pressure, cholesterol, and obesity for prediction. The EHDPS predicts the likelihood of patients getting heart disease. It enables significant knowledge, eg, relationships between medical factors related to heart disease and patterns, to be established. We have employed the multilayer perceptron neural network with backpropagation as the training algorithm. The obtained results have illustrated that the designed diagnostic system can effectively predict the risk level of heart diseases.**
-         """)
-st.image("https://media.istockphoto.com/vectors/3d-realistic-vector-isolated-human-heart-anatomically-correct-heart-vector-id1148910293?k=20&m=1148910293&s=612x612&w=0&h=9jvKArN3eCFYX92LINaUgLKkFlDp3xVbIrf00W6YQ3U=")
+         **I have explored how people are travelling from different stops in Adelaide Metropolitan area and the rate at which passengers on each bus route are increasing.finally created a predictive model to find the load of passengers on public Bus transport system in future..** """)
+st.image("https://thumbs.dreamstime.com/z/watch-out-buses-kalakad-tamil-nadu-india-circa-streets-crowded-dominating-bus-india-s-population-more-than-32158551.jpg")
 
 
 st.header('**SAMPLE DATASET**')
-column = ['age','sex']
+
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/1XrrI0gSfZ0XoM98MT-k9JNfov8dHnsxdm7efwuRcvTY/edit#gid=0", names = column);
 st.table(data)
 
-st.header('**DATA STUDIO**')
-st.image("https://media.istockphoto.com/photos/heart-medical-exam-picture-id1306663325?b=1&k=20&m=1306663325&s=170667a&w=0&h=p4in-M2wMo9mBueTVWuYnX2lHL6BI9qsFDnGTrNhkk4=")
-st.image("https://cdn-images-1.medium.com/max/1600/1*PkEl-8DBQa-xEft_tacXLQ.gif")
+t.write("**Thank You for your wonderful time**")
 
-st.write("**Thank You for your wonderful time**")
-st.write("To get the source code click on the [link]https://colab.research.google.com/drive/14YZQ2swFnimLR38cwTWnz7TezC_NOrus?usp=sharing")", names = column);
-st.table(data)
-
-st.header('**DATA STUDIO**')
-st.image("https://media.istockphoto.com/photos/heart-medical-exam-picture-id1306663325?b=1&k=20&m=1306663325&s=170667a&w=0&h=p4in-M2wMo9mBueTVWuYnX2lHL6BI9qsFDnGTrNhkk4=")
-st.image("https://cdn-images-1.medium.com/max/1600/1*PkEl-8DBQa-xEft_tacXLQ.gif")
-
-st.write("**Thank You for your wonderful time**")
-st.write("To get the source code click on the [link]https://colab.research.google.com/drive/14YZQ2swFnimLR38cwTWnz7TezC_NOrus?usp=sharing")
