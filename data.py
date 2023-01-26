686c07feb797661918edc8958cd13999ba797759
roshanmahendravarman <123367901+roshanmahendravarman@users.noreply.github.com>
2023-01-26 16:42:51 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,58 +1,3 @@
-
-import setuptools
-
-setuptools.setup(
-    name="streamlit-option-menu",
-    version="0.3.3",
-    author="Victor Yan",
-    author_email="victoryhb@163.com",
-    description="streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.",
-    long_description="""streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.
-It is similar in function to st.selectbox(), except that:
-- It uses a simple static list to display the options instead of a dropdown
-- It has configurable icons for each option item and the menu title
-It is built on [streamlit-component-template-vue](https://github.com/andfanilo/streamlit-component-template-vue), styled with [Bootstrap](https://getbootstrap.com/) and with icons from [bootstrap-icons](https://icons.getbootstrap.com/)
-## Installation
-```
-pip install streamlit-option-menu
-```
-## Parameters
-The `option_menu` function accepts the following parameters:
-- menu_title (required): the title of the menu; pass None to hide the title
-- options (required): list of (string) options to display in the menu; set an option to "---" if you want to insert a section separator
-- default_index (optional, default=0): the index of the selected option by default
-- menu_icon (optional, default="menu-up"): name of the [bootstrap-icon](https://icons.getbootstrap.com/) to be used for the menu title
-- icons (optional, default=["caret-right"]): list of [bootstrap-icon](https://icons.getbootstrap.com/) names to be used for each option; its length should be equal to the length of options
-- orientation (optional, default="vertical"): "vertical" or "horizontal"; whether to display the menu vertically or horizontally
-The function returns the (string) option currently selected
-## Example
-```
-import streamlit as st
-from streamlit_option_menu import option_menu
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-# horizontal Menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-    selected2
-```
-""",
-    long_description_content_type="text/plain",
-    url="https://github.com/victoryhb/streamlit-option-menu",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[],
-    python_requires=">=3.6",
-    install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
-        "streamlit >= 0.63",
-    ],
-)
 import streamlit as st
 import pickle
 import pandas as pd
@@ -69,3 +14,94 @@ selected = option_menu(
        default_index = 0,
        orientation = "horizontal",  #to make the menubar horizontal 
     )
+    
+if selected == "Home":
+    #st.title('Home')
+    with st.container():
+        st.title('Customer churn finder for telco company!')
+        st.subheader('By Anirudh :wave:')
+    
+    with st.container():
+        st.title('What is customer churn :information_source:')
+        st.write('When a customer stops doing business with the company the customer is churned.The buisness are keen in keeping the existing customer because it is far less resource consuming than aquiring a new customer. Existing customers will often have a higher volume of service consumption and can generate additional customer referrals. Customer churn can be resovled by giving a good product and services.Preventing customer churn is critically important in telecommunication sector in retail as barriers to entry switching services and provider are so low. Acquire different datasets from kaggle, IBM sample datasets, Google dataset sample etc.. ')
+        st.image("https://cdn.technologyadvice.com/wp-content/uploads/2020/03/6-Ways-CRM-Stop-Customer-Churn.png")
+        st.subheader('This is a sample Dataset:')
+        st.text('In the column ContractRenewal and DataPlan 1 = Yes and 0 = No')
+        df = pd.read_csv("telecom_churn.csv")
+        st.write(df)
+
+
+if selected == "Model":
+    #st.title('Model')
+    st.title('Customer Churn')
+    st.subheader('Enter the details')
+ 
+    model = st.selectbox('choose a model',
+      ('Decision tree classification', 'Random forest classification', 'Naive Bayes Classification'))
+
+    if model =='Decision tree classification':
+       m = pickle.load(open('decisiontree_sm.sav', 'rb'))
+    elif model =='Random forest classification':
+       m = pickle.load(open('randomforest_sm.sav', 'rb'))
+    else:
+       m = pickle.load(open('naivebayes_sm.sav', 'rb'))
+
+
+    def ccp(input_data):
+       na = np.asarray(input_data)
+       d = na.reshape(1, -1)
+
+       prediction = m.predict(d)
+       print(prediction)
+
+       if (prediction[0] == 0):
+        return 'the customer is not churned'
+       else:
+        return 'the customer is churned'
+
+
+    def main():
+       
+     AccountWeeks = st.text_input('Account Weeks')
+     ContractRenewal_1 = st.selectbox(
+        'ContractRenewal', ('Yes', 'No'))
+     DataPlan_1 = st.selectbox(
+        'DataPlan', ('Yes', 'No'))
+
+     #ContractRenewal = st.text_input('Contract Renewal')
+     #DataPlan = st.text_input('Data Plan')
+
+     DataUsage = st.text_input('Data Usage')
+     CustServCalls = st.text_input('Customer Service Call')
+     DayMins = st.text_input('Day Mins')
+     DayCalls = st.text_input('Day Calls')
+     MonthlyCharge = st.text_input('Monthly Charge')
+     OverageFee = st.text_input('Overage Fee')
+     RoamMins = st.text_input('Roam Mins')
+
+     if ContractRenewal_1 == 'Yes':
+        ContractRenewal = 1
+     else:
+        ContractRenewal = 0
+
+     if DataPlan_1 == 'Yes':
+        DataPlan = 1
+     else:
+        DataPlan = 0 
+  
+     cc = ''
+
+     if st.button('Get Results'):
+       cc = ccp([AccountWeeks,ContractRenewal,DataPlan,DataUsage,CustServCalls,
+                DayMins,DayCalls,MonthlyCharge,OverageFee,RoamMins])
+       st.success(cc)
+
+    if __name__ == '__main__':
+      main()
+
+if selected == "About":
+    #st.title('About')
+    st.title('Hi, Anirudh here :smiley:')
+    st.subheader('Hope this app was useful to you :innocent:')
+    st.write('I am a student as of now who is passionate about Data Science and this is my first project. This customer churn app uses three classification models namely Decision tree, Random forest and Naive Bayes from which you can select one just like the best of three when we used to play stone paper scissors. fill in all the details just as in the data set and get the output.')
+    st.write('[More projects in GITHUB >](https://github.com/4nirudh5)')
