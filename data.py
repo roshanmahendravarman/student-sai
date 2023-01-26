7fc9ed3a150e871524d8b5b250e223e321720a98
roshanmahendravarman <123367901+roshanmahendravarman@users.noreply.github.com>
2023-01-26 16:32:45 +0530

    Update data.py

--- data.py
+++ data.py
@@ -17,14 +17,14 @@ _RELEASE = True
 # best practice.
 
 if not _RELEASE:
-    _component_func = components.declare_component(
+    component_func = components.declare_component(
         "option_menu",
         url="http://localhost:3001",
     )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/dist")
-    _component_func = components.declare_component(
+    component_func = components.declare_component(
         "option_menu", path=build_dir)
 
 # Create a wrapper function for the component. This is an optional
